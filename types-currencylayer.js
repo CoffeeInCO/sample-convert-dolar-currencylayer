403a1a5e917b2504ddd3edc638b37ba3c8830831
Bruno G. Souza <bruno@bgsouza.com>
2018-02-18 21:31:59 -0300

    Create types-currencylayer.js

---
+++ types-currencylayer.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CurrencyLayerType;
+(function (CurrencyLayerType) {
+    CurrencyLayerType[CurrencyLayerType["HISTORICAL"] = 1] = "HISTORICAL";
+    CurrencyLayerType[CurrencyLayerType["LIVE"] = 2] = "LIVE";
+})(CurrencyLayerType = exports.CurrencyLayerType || (exports.CurrencyLayerType = {}));
